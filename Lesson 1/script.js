1e16813d6cebf11c82055688efefab42cfefd7e3
Valeriy <skyvalery70@gmail.com>
2018-10-04 16:12:41 +0300

    Этой мое первое дз

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,1 +1,20 @@
-var = "Валерий";
+// var yourBudget = +prompt("Ваш бюджет?", "10000");
+// var nameShop = prompt("Название вашего магазина", "Мой магазин");
+// console.log(budget);
+// console.log(nameShop);
+
+var mainList = {
+	budget: +prompt("Ваш бюджет?", "10000"),
+	shopName: prompt("Название вашего магазина", "Мой магазин"),
+	shopGoods: [],
+	employers: {},
+	open: true
+};
+
+
+for(let i = 0; i < 3; i++){
+	mainList.shopGoods[i] = prompt("Какой тип товаров будем продавать?", "тип товара");
+}
+
+console.log("Ваш бюджет на 1 день: " + mainList.budget / 30);
+
