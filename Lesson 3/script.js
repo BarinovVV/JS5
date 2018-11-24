ae7670dc41d1841a16365b277ac69106b6734728
Valeriy <skyvalery70@gmail.com>
2018-11-24 13:35:08 +0300

    Домашнее задание № 3

--- Lesson 3/script.js
+++ Lesson 3/script.js
@@ -53,8 +53,8 @@ let money = +prompt('Ваш бюджет на месяц', '10000'),
 
 let i = 0;
 do {
-	let a = prompt('Введите первую обязательную статью расходов', ''),
-			b = prompt('Во сколько она вам обойдется', '');
+	let a = prompt('Введите первую обязательную статью расходов', 'Статья № '),
+			b = +prompt('Во сколько она вам обойдется', '100');
 			console.log(typeof(a));
 			console.log(typeof(b));
 				if (a != null && b != null && a != '' && b !='' && a.length < 50) {
@@ -68,22 +68,45 @@ do {
 			i++;
 } while (i < 2);
 
-appData.budgetPerDay = (appData.yourBudget/30).toFixed(2);
-
-document.writeln('Ваш бюджет на один день составляет ' + appData.budgetPerDay);
-
-
-let level = +appData.budgetPerDay;
-
-if (level < 100) {
-	console.log('Минимальный уровень');
-} else if (level > 100 && level < 2000) {
-	console.log('Средний уровень');
-} else if (level > 2000) {
-	console.log('Высокий уровень');
-} else {
-	console.log('Уровень невозможно определить');
+//Определение функции по выводу бюджета
+function detectDayBudget() {
+	appData.budgetPerDay = +(appData.yourBudget/30).toFixed(2);
+	document.writeln('Ваш бюджет на один день составляет ' + appData.budgetPerDay);
 }
+
+//вызов функции и вывод бюджета на день на экран
+detectDayBudget();
+
+//Определение функции по определению уровня дохода
+function detectLevel(level) {
+	if (level < 100) {
+		return console.log('Минимальный уровень');
+	} else if (level > 100 && level < 2000) {
+		return console.log('Средний уровень');
+	} else if (level > 2000) {
+		return console.log('Высокий уровень');
+	} else {
+		return console.log('Уровень невозможно определить');
+	}
+}
+
+// let level = +appData.budgetPerDay;
+
+//вызов функции и вывод уровня дохода в консоль
+detectLevel(+appData.budgetPerDay);
+
+
+//Определение функции по введению необязательных расходов
+function chooseOptExpenses() {
+	for (i = 1; i <= 3; i++) {
+		let answer = prompt("Введите статью необязательных расходов", "Статья")
+		appData.optionalExpenses[i] = answer;
+	}
+}
+
+//Вызов функции по введению необязательных расходов
+chooseOptExpenses();
+
  
 
 // switch (level) {
