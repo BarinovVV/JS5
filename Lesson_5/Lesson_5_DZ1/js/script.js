5962d2a637ba260a973d80e868820ec3bd392914
Valeriy <skyvalery70@gmail.com>
2018-11-26 15:00:06 +0300

    Коммит с ДЗ №5

--- Lesson_5/Lesson_5_DZ1/js/script.js
+++ Lesson_5/Lesson_5_DZ1/js/script.js
@@ -5,7 +5,10 @@ console.log("Начинаем делать урок 5");
 let menuItems = document.querySelectorAll('.menu-item'),
 		menu = document.querySelector('.menu'),
 		main = document.getElementsByTagName('body'),
-		title = document.querySelector('#title');
+		title = document.querySelector('#title'),
+		adv = document.querySelector('.adv'),
+		pro = document.querySelector('.prompt');
+
 
 
 let arr = [];
@@ -18,8 +21,17 @@ menuItems[2].innerText = 'Третий пункт';
 
 main[0].style.backgroundImage = 'url(../Lesson_5_DZ1/img/apple_true.jpg)';
 
+// menuItems[3].cloneNode(true);
 let li = document.createElement('li');
 li.classList.add('menu-item');
 li.innerText = 'Пятый пункт';
 menu.appendChild(li);
-title.innerHTML = "Мы продаем только <strong>подлинную</strong> технику Apple";
+title.innerHTML = "Мы продаем только <strong><ins>подлинную</ins></strong> технику Apple";
+
+adv.remove();
+
+let relation = prompt("Как вы относитесь к технике Apple?", "Качественная и надежная");
+
+pro.innerText = relation;
+pro.style.fontSize = '30px';
+pro.style.color = 'yellow';
