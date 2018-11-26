b27c7f9f89ad86940d03d061f6d5d7f327e1d57c
Valeriy <skyvalery70@gmail.com>
2018-11-26 14:28:23 +0300

    Коммит с ДЗ №5

--- Lesson_5/Lesson_5_DZ1/js/script.js
+++ Lesson_5/Lesson_5_DZ1/js/script.js
@@ -2,17 +2,24 @@
 
 console.log("Начинаем делать урок 5");
 
-let menu = document.querySelectorAll('.menu-item'),
-		main = document.getElementsByTagName('body');
+let menuItems = document.querySelectorAll('.menu-item'),
+		menu = document.querySelector('.menu'),
+		main = document.getElementsByTagName('body'),
+		title = document.querySelector('#title');
 
 
 let arr = [];
-menu.forEach(function(item, i) {
-	console.log(menu[i].innerText);
-	arr.push(menu[i].innerText);
+menuItems.forEach(function(item, i) {
+	console.log(menuItems[i].innerText);
+	arr.push(menuItems[i].innerText);
 });
-menu[1].innerText = 'Второй пункт';
-menu[2].innerText = 'Третий пункт';
-// menu[4].innerText = 'Пятый пункт';
+menuItems[1].innerText = 'Второй пункт';
+menuItems[2].innerText = 'Третий пункт';
 
-main[0].style.backgroundImage = 'url(../Lesson%205%20DZ1/img/apple_true.jpg)';
+main[0].style.backgroundImage = 'url(../Lesson_5_DZ1/img/apple_true.jpg)';
+
+let li = document.createElement('li');
+li.classList.add('menu-item');
+li.innerText = 'Пятый пункт';
+menu.appendChild(li);
+title.innerHTML = "Мы продаем только <strong>подлинную</strong> технику Apple";
