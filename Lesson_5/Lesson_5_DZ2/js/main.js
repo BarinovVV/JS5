1a7babdf5bad43a898a9c54663a215b662d62fd5
Valeriy <skyvalery70@gmail.com>
2018-11-27 14:47:53 +0300

    Коммит с ДЗ №5

--- Lesson_5/Lesson_5_DZ2/js/main.js
+++ Lesson_5/Lesson_5_DZ2/js/main.js
@@ -2,26 +2,37 @@
 
 // console.log("Начинаем делать урок 5 задание 2");
 
-let startBtn = document.getElementByID('start'),
+let startBtn = document.getElementById('start'),
 		budgetValue = document.getElementsByClassName('budget-value')[0],
 		dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
 		levelValue = document.getElementsByClassName('level-value')[0],
 		expensesValue = document.getElementsByClassName('expenses-value')[0],
 		optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
 		incomeValue = document.getElementsByClassName('income-value')[0],
-		incomeValue = document.getElementsByClassName('income-value')[0],
-		monthSavingsValue = document.getElementsByClassName('monthSavings-value')[0],
 		monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
 		yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-		
+		expensesItem = document.getElementsByClassName('expenses-item'),
+		optionalExpensesItem = document.querySelectorAll('optionalexpenses-item'),
 
-		values = document.querySelectorAll('div[Class$="value"]'),
-		inputs = document.querySelectorAll('input[Class="expenses-item"]'),
-		inputsOpt = document.querySelectorAll('input[Class="optionalexpenses-item"]'),
-		approveBtn = document.querySelector('button[Class="expenses-item-btn"]'),
-		approveOptBtn = document.querySelector('button[Class="optionalexpenses-btn"]'),
-		calculateBtn = document.querySelector('button[Class="count-budget-btn"]');
+		expensesBtn = document.getElementsByTagName('button')[0],
+		optionalExpensesBtn = document.getElementsByTagName('button')[0],
+		countBtn = document.getElementsByTagName('button')[0],
+
+		incomeItem = document.querySelector('choose-income'),
+		checkSavings = document.querySelector('#savings'),
+		sumValue = document.querySelector('.choose-sum'),
+		percentValue = document.querySelector('.choose-percent'),
+		yearValue = document.querySelector('.year-value'),
+		monthValue = document.querySelector('.month-value'),
+		dayValue = document.querySelector('.day-value');
+
+		// values = document.querySelectorAll('div[Class$="value"]'),
+		// inputs = document.querySelectorAll('input[Class="expenses-item"]'),
+		// inputsOpt = document.querySelectorAll('input[Class="optionalexpenses-item"]'),
+		// approveBtn = document.querySelector('button[Class="expenses-item-btn"]'),
+		// approveOptBtn = document.querySelector('button[Class="optionalexpenses-btn"]'),
+		// calculateBtn = document.querySelector('button[Class="count-budget-btn"]');
 
 
 
