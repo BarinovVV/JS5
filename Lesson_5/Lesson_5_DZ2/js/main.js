25956c64557af2696146f81275565babde9b9eea
Valeriy <skyvalery70@gmail.com>
2018-11-27 08:51:26 +0300

    Коммит с ДЗ №5

--- Lesson_5/Lesson_5_DZ2/js/main.js
+++ Lesson_5/Lesson_5_DZ2/js/main.js
@@ -2,7 +2,20 @@
 
 // console.log("Начинаем делать урок 5 задание 2");
 
-let btnStart = document.querySelector('#start'),
+let startBtn = document.getElementByID('start'),
+		budgetValue = document.getElementsByClassName('budget-value')[0],
+		dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+		levelValue = document.getElementsByClassName('level-value')[0],
+		expensesValue = document.getElementsByClassName('expenses-value')[0],
+		optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+		incomeValue = document.getElementsByClassName('income-value')[0],
+		incomeValue = document.getElementsByClassName('income-value')[0],
+		monthSavingsValue = document.getElementsByClassName('monthSavings-value')[0],
+		monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+		yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+		
+
 		values = document.querySelectorAll('div[Class$="value"]'),
 		inputs = document.querySelectorAll('input[Class="expenses-item"]'),
 		inputsOpt = document.querySelectorAll('input[Class="optionalexpenses-item"]'),
