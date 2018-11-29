99814f9b4d557ae57fb2afb6accd78cbbfe8af36
Valeriy <skyvalery70@gmail.com>
2018-11-29 13:03:48 +0300

    Коммит с ДЗ №7

---
+++ Lesson_7/js/timer.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
+
+	let timerText = document.querySelector('.timer');
+
+
+// function currentTime() {
+// 		let now = new Date();
+// 		timerText.textContent = now.toLocaleTimeString();
+// }
+
+// let timer = setInterval(currentTime, 1000);
+
+let timer = setTimeout(function currentTime() {
+	let now = new Date();
+			timerText.textContent = now.toLocaleTimeString();
+			setTimeout(currentTime, 1000);
+});
+
+
+});
