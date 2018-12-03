987ab21dc6683f3016ad58e181095e9f42fd8034
Valeriy <skyvalery70@gmail.com>
2018-12-03 14:26:04 +0300

    Коммит с ДЗ №10

---
+++ Lesson_10/js/script.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
+
+	let lesson = document.querySelector('h2'),
+			title = document.querySelector('title'),
+			myBody = document.querySelector('body');
+
+	lesson.textContent = 'Домашнее задание Урок № 10';
+	title.textContent = 'Lesson 10';
+	lesson.style.textAlign = 'center';
+
+	class Options {
+		constructor(height, width, bg, fontSize, textAlign) {
+			this.height = height;
+			this.width = width;
+			this.bg = bg;
+			this.fontSize = fontSize;
+			this.textAlign = textAlign;
+		}
+
+		createDiv() {
+			let newDiv = document.createElement('div');
+
+			myBody.appendChild(newDiv);
+			newDiv.textContent = 'Мы создали новый div';
+			newDiv.style.cssText = `height: ${this.height}px; \
+			width: ${this.width}px; \
+			background-color: ${this.bg};\
+			font-size: ${this.fontSize}px;
+			text-align: ${this.textAlign};
+			`;
+
+
+		}
+
+
+	}
+
+	let newDiv = new Options(200, 500, 'red', 30, 'center',);
+
+	newDiv.createDiv();
+
+
+});
