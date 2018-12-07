0057ed8faaecb20935b58d8b8c41b49a7eb1d1a6
Valeriy <skyvalery70@gmail.com>
2018-12-07 15:14:38 +0300

    Коммит с ДЗ №13

--- Lesson_13/js/script.js
+++ Lesson_13/js/script.js
@@ -249,14 +249,45 @@ window.addEventListener('DOMContentLoaded', () => {
         dots = document.querySelectorAll('.dot');
 
     showSlides(slideIndex);
+
     function showSlides(n) {
+        
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        
         slides.forEach(item => item.style.display = 'none');
         dots.forEach(item => item.classList.remove('dot-active'));
 
         slides[slideIndex - 1].style.display = 'block';
         dots[slideIndex - 1].classList.add('dot-active');
-
     }
+
+    function plusSlide(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', () =>  {
+        plusSlide(-1);
+    });
+
+    next.addEventListener('click', () =>  {
+        plusSlide(1);
+    });
+
+    dotsWrap.addEventListener('click', event => {
+       for (let i = 1; i < dots.length + 1; i++) {
+           if (event.target.classList.contains('dot') && event.target == dots[i-1] ) {
+                currentSlide(i);
+           }
+       }
+    });
 
 });
 
