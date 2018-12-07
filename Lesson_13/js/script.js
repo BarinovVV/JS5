4f96a8a8a7822dfdad207c5c2448da1fc68b5403
Valeriy <skyvalery70@gmail.com>
2018-12-07 15:38:50 +0300

    Коммит с ДЗ №13

--- Lesson_13/js/script.js
+++ Lesson_13/js/script.js
@@ -289,5 +289,30 @@ window.addEventListener('DOMContentLoaded', () => {
        }
     });
 
+    //calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total =0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' ) {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    })
+
+
+
 });
 
